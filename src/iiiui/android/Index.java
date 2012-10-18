5d32ef5e5dc2b2f7221187b1d406dcd7d32f70c5
han evan <your_email@youremail.com>
2012-10-18 18:32:17 +0800

    update menu

--- src/iiiui/android/Index.java
+++ src/iiiui/android/Index.java
@@ -2,6 +2,10 @@ package iiiui.android;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class Index extends Activity {
@@ -10,4 +14,39 @@ public class Index extends Activity {
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.index);
 	}
+	
+	/**
+	* 重写onCreateOptionsMenu 方法
+	*/
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		//获取MenuInflater对象
+		MenuInflater inflater = getMenuInflater();
+		// 使用自定义的XML菜单文件
+		inflater.inflate(R.layout.menu, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+
+	/**
+	* 判断所选择的菜单
+	*/
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		switch (item.getItemId())
+		{
+			case R.id.menu_index:
+			Toast.makeText(this, "选择了主页", Toast.LENGTH_LONG).show();
+			break;
+			
+			case R.id.menu_share:
+			Toast.makeText(this, "选择了分享", Toast.LENGTH_LONG).show();
+			break;
+			
+			default:
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
 }
